3e02ec22685f24e7c2954f6748f4e8366d3435d8
Mani Sandoval <inkandpaperworlds21@gmail.com>
2018-03-02 07:52:29 -0600

    Conexión
    Conexión a la base de datos.

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+  $db = 'rws';
+  $host = 'localhost';
+  $user = 'root';
+  $pass = '';
+
+  $conexion = mysqli_connect($host, $user, $pass);
+  mysqli_select_db($conexion, $db);
+
+
+
+ ?>
