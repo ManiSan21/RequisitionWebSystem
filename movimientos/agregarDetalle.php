390c44aeb5c279574fa3adc2a78df8ca52056c5c
diego0309 <diegoosvaldo0309@gmail.com>
2018-03-31 11:34:25 -0600

    ya agrega al detalle

--- movimientos/agregarDetalle.php
+++ movimientos/agregarDetalle.php
@@ -1,11 +1,23 @@
 <?php
+  include "../conexion.php";
   session_start();
   $_SESSION['compra'][$_SESSION['contador']][0] = $_POST['materiales'];
   $_SESSION['compra'][$_SESSION['contador']][1] = $_POST['cantidad'];
   $_SESSION['compra'][$_SESSION['contador']][2] = $_POST['precio'];
 
-  echo "<tr>
+  $sql = "SELECT IdMaterial FROM materiales WHERE Nombre='".$_POST['materiales']."'";
+  $nombre = "Hola";
+  $importe = $_POST['cantidad']*$_POST['precio'];
+  $result = mysqli_query($conexion, $sql);
+  while($row = mysqli_fetch_row($result))
+  {
+    $nombre = $row[0];
+  }
+  echo "<tr class=active>
        <td>
+          ".$nombre."
+        </td>
+        <td>
           ".$_POST['materiales']."
         </td>
         <td>
@@ -13,6 +25,9 @@
         </td>
         <td>
           ".$_POST['precio']."
+        </td>
+        <td>
+          ".$importe."
         </td>
   </tr>";
 
