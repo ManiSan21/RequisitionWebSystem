d9a9f1ba7fa36f156c62d9aeb10a99d142a2077a
ManiSan21 <inkandpaperworlds21@gmail.com>
2018-05-07 23:20:43 -0500

    Cambios al nav

--- indexAdmin.php
+++ indexAdmin.php
@@ -10,7 +10,8 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/estiloMenu.css" />
-    <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <title>RWS - Página principal</title>
   </head>
   <body>
@@ -25,12 +26,31 @@
           <li><a title="compraMateriales" href="movimientos/compraMaterialesF.php">Registrar Compras de materiales</a></li>
           <li><a title="solicitudesServicio" href="movimientos/RegistroSolicitudesForm.php">Registrar solicitud de servicio</a></li>
           <li><a title="solucionSolicitudes" href="movimientos/SolucionSolicitudesForm.php">Registrar solución de solicitudes</a></li>
-          <!--<li>
-            Consultas
-            <ul>
-              <li><a title="consultaCompras" href="consultas/ConsultaCompras.php">Consulta de compras</a></li>
-            </ul>
-          </li>-->
+          <div class="dropdown">
+            <button class="dropbtn">Consultas
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div id="consultas" class="dropdown-content">
+              <a href="consultas/ConsultaSolicitudes.php">Consulta de solicitudes</a>
+              <a href="consultas/ConsultaCompras.php">Consulta de Compras</a>
+              <a href="consultas/ConsultaSoluciones.php">Consulta de Soluciones</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <button class="dropbtn">Reportes
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div id="reportes" class="dropdown-content">
+              <a href="reportes/reporteAulas.php">Reporte de Aulas</a>
+              <a href="reportes/reporteMateriales.php">Reporte de Materiales</a>
+              <a href="reportes/reporteProveedores.php">Reporte de Proveedores</a>
+              <a href="reportes/reporteServicios.php">Reporte de Servicios</a>
+              <a href="reportes/reporteSolicitudes.php">Reporte de Solicitudes</a>
+              <a href="reportes/reporteSolicitudesPen.php">Reporte de Solicitudes pendientes</a>
+              <a href="reportes/reporteSolicitudesSol.php">Reporte de Solicitudes solucionadas</a>
+              <a href="reportes/reporteSoluciones.php">Reporte de Soluciones</a>
+            </div>
+          </div>
           <li>Usuario en sesion: <?php echo $usuario; ?></li>
           <li><a title="cerrarSesion" name="cerrarSession" href="cerrarSesion.php">Cerrar Sesión</a></li>          
       </ul>
--- nav.php
+++ nav.php
@@ -10,8 +10,8 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="css/estiloMenu.css" />
-    <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+    <!--<link rel="stylesheet" type="text/css" href="../css/estiloMenu.css" />
+    <link rel="stylesheet" type="text/css" href="../css/estilos.css">-->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <title>RWS - Página principal</title>
   </head>
@@ -28,9 +28,28 @@
       <li><a title="solicitudesServicio" href="../movimientos/RegistroSolicitudesForm.php">Registrar solicitud de servicio</a></li>
       <li><a title="solucionSolicitudes" href="../movimientos/SolucionSolicitudesForm.php">Registrar solución de solicitudes</a></li>
       <div class="dropdown">
-        <button onclick="miFuncion()" class="">Reportes</button>
+        <button class="dropbtn">Consultas
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div id="consultas" class="dropdown-content">
+          <a href="../consultas/ConsultaSolicitudes.php">Consulta de solicitudes</a>
+          <a href="../consultas/ConsultaCompras.php">Consulta de Compras</a>
+          <a href="../consultas/ConsultaSoluciones.php">Consulta de Soluciones</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">Reportes
+          <i class="fa fa-caret-down"></i>
+        </button>
         <div id="reportes" class="dropdown-content">
-          <a href="../reportes/reporteAulas.php"></a>
+          <a href="../reportes/reporteAulas.php">Reporte de Aulas</a>
+          <a href="../reportes/reporteMateriales.php">Reporte de Materiales</a>
+          <a href="../reportes/reporteProveedores.php">Reporte de Proveedores</a>
+          <a href="../reportes/reporteServicios.php">Reporte de Servicios</a>
+          <a href="../reportes/reporteSolicitudes.php">Reporte de Solicitudes</a>
+          <a href="../reportes/reporteSolicitudesPen.php">Reporte de Solicitudes pendientes</a>
+          <a href="../reportes/reporteSolicitudesSol.php">Reporte de Solicitudes solucionadas</a>
+          <a href="../reportes/reporteSoluciones.php">Reporte de Soluciones</a>
         </div>
       </div>
       <li>Usuario en sesion: <?php echo $usuario; ?></li>      
